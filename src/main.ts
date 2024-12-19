928073274389c6b8a35fd06dc0962bcfbb1e4338
Fromsko <99723642+Fromsko@users.noreply.github.com>
2024-12-19 09:59:26 +0000

    fix: change import path alias

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// import "~/styles/element/index.scss";
+// import "@/styles/element/index.scss";
 
 // import ElementPlus from "element-plus";
 // import all element css, uncommented next line
@@ -9,7 +9,7 @@ import App from "./App.vue";
 
 // or use cdn, uncomment cdn link in `index.html`
 
-import "~/styles/index.scss";
+import "@/styles/index.scss";
 import "uno.css";
 
 // If you want to use ElMessage, import it.
